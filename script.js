4fbc60b40ddcc0b788b5d4a2e412c3aa0142ca80
MohamedImran25 <139478403+MohamedImran25@users.noreply.github.com>
2023-08-24 02:12:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function changenext(){
+    document.getElementById("image").src ="Travel-Wallpaper-Background.webp";
+}
+function changeprevious(){
+    document.getElementById("image").src ="maxresdefault.jpg";
+}
